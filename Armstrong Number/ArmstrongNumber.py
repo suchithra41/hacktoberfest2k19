b4dc4422b2400a44136df3c4730e8beabf9cfa18
suchithra41 <56391403+suchithra41@users.noreply.github.com>
2019-10-20 01:32:58 +0530

    Update ArmstrongNumber.py

--- Armstrong Number/ArmstrongNumber.py
+++ Armstrong Number/ArmstrongNumber.py
@@ -1,7 +1,12 @@
-number, new_number  = input("Enter a number to check if it is an Armstrong Number: "), 0
-for i in number:
-    new_number += int(i) ** 3
-if number == new_number:
-    print("The number is an Armstrong number")
-else:
-    print("It is not an Armstrong Number")
+import math
+def ans(x):
+    n=order(x)
+    t_v=x
+    sum1=0
+    while(t_v!=0):
+        a=t_v%10
+        n=3
+        sum1=sum1+math.pow(a,n)
+        t_v=t_v/10
+    return(sum1==x)
+print(ans(x))
